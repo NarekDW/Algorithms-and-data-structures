efac4f82b717dd9de6700f961c73e0824ae671f1
narek_karapetian <narek.karapetian93@yandex.ru>
2022-10-02 18:41:19 +0400

    fix 1.3.38

--- src/_1_Fundamentals/_1_3_Bags_Queues_and_Stacks/creative/GeneralizedQueue.java
+++ src/_1_Fundamentals/_1_3_Bags_Queues_and_Stacks/creative/GeneralizedQueue.java
@@ -1,15 +1,15 @@
 package _1_Fundamentals._1_3_Bags_Queues_and_Stacks.creative;
 
 /*****************************************************************************************************
- *
+ * <p>
  * 1.3.38 Delete k th element. Implement a class that supports the following API:
- *
+ * <p>
  * public class GeneralizedQueue<Item>
  *      GeneralizedQueue() - create an empty queue
  *      boolean isEmpty() - is the queue empty?
  *      void insert(Item x) add an item
  *      Item delete(int k) delete and return the k th least recently inserted item
- *
+ * <p>
  * First, develop an implementation that uses an array implementation, and then develop
  * one that uses a linked-list implementation. Note : the algorithms and data structures
  * that we introduce in Chapter 3 make it possible to develop an implementation that
@@ -20,14 +20,11 @@ package _1_Fundamentals._1_3_Bags_Queues_and_Stacks.creative;
 public class GeneralizedQueue<T> {
     private T[] q;
     private int n;
-    private int first;
-    private int last;
 
     public GeneralizedQueue() {
+        //noinspection unchecked
         q = (T[]) new Object[2];
         n = 0;
-        first = 0;
-        last = 0;
     }
 
     public boolean isEmpty() {
@@ -41,14 +38,14 @@ public class GeneralizedQueue<T> {
     public void insert(T item) {
         if (n == q.length)
             resize(n * 2);
-        q[last++] = item;
-        if (last == q.length)
-            last = 0;
-        n++;
+        q[n++] = item;
     }
 
     public T delete(int k) {
-        if (n == q.length / 4) resize(q.length / 2);
+        if (n == 0 || k > n)
+            return null;
+        if (n == q.length / 4)
+            resize(q.length / 2);
         T item = q[k];
         for (int i = k; i < n - 1; i++)
             q[i] = q[i + 1];
@@ -58,32 +55,46 @@ public class GeneralizedQueue<T> {
     }
 
     private void resize(int max) {
+        //noinspection unchecked
         T[] tmp = (T[]) new Object[max];
+        //noinspection ManualArrayCopy
         for (int i = 0; i < n; i++)
-            tmp[i] = q[(first + i) % q.length];
-        first = 0;
-        last = n;
+            tmp[i] = q[i];
         q = tmp;
     }
 
     public static void main(String[] args) {
-        GeneralizedQueue<Integer> queue = new GeneralizedQueue<>();
-        queue.insert(1);
-        queue.insert(2);
-        queue.insert(3);
-        queue.insert(4);
-        queue.print();
-        System.out.println();
-        System.out.println(queue.delete(1));
-        System.out.println(queue.delete(1));
+        System.out.println("Running test for GeneralizedQueue.");
+        GeneralizedQueue<Integer> queue = generateQueue(15);
+        if (queue.isEmpty())
+            throw new RuntimeException("isEmpty");
 
-        queue.print();
+        for (int i = 0; i < 15; i++)
+            if (queue.delete(0) != i)
+                throw new RuntimeException(String.valueOf(i));
 
+        if (!queue.isEmpty())
+            throw new RuntimeException("!isEmpty");
+
+        queue = generateQueue(15);
+        if (queue.delete(5) != 5)
+            throw new RuntimeException("delete != 6");
+        if (queue.delete(11) != 12)
+            throw new RuntimeException("delete != 12");
+        if (queue.delete(12) != 14)
+            throw new RuntimeException("delete != 14");
+        queue.insert(33);
+        if (queue.delete(12) != 33)
+            throw new RuntimeException("delete != 33");
+        if (queue.delete(100) != null)
+            throw new RuntimeException("delete != null");
     }
 
-    private void print() {
-        for (T t : q) {
-            System.out.print(t + " ");
-        }
+    @SuppressWarnings("SameParameterValue")
+    private static GeneralizedQueue<Integer> generateQueue(int n) {
+        GeneralizedQueue<Integer> queue = new GeneralizedQueue<>();
+        for (int i = 0; i < n; i++)
+            queue.insert(i);
+        return queue;
     }
 }
--- src/_1_Fundamentals/_1_3_Bags_Queues_and_Stacks/creative/GeneralizedQueueLinked.java
+++ src/_1_Fundamentals/_1_3_Bags_Queues_and_Stacks/creative/GeneralizedQueueLinked.java
@@ -2,15 +2,15 @@ package _1_Fundamentals._1_3_Bags_Queues_and_Stacks.creative;
 
 
 /*****************************************************************************************************
- *
+ * <p>
  * 1.3.38 (GeneralizedQueueLinked) Delete k th element. Implement a class that supports the following API:
- *
+ * <p>
  * public class GeneralizedQueue<Item>
  *      GeneralizedQueue() - create an empty queue
  *      boolean isEmpty() - is the queue empty?
  *      void insert(Item x) add an item
  *      Item delete(int k) delete and return the k th least recently inserted item
- *
+ * <p>
  * First, develop an implementation that uses an array implementation, and then develop
  * one that uses a linked-list implementation. Note : the algorithms and data structures
  * that we introduce in Chapter 3 make it possible to develop an implementation that
@@ -43,7 +43,25 @@ public class GeneralizedQueueLinked<T> {
     }
 
     public T delete(int k) {
+        if (n == 0 || k > n)
+            return null;
+        n--;
         Node tmp = left;
+        // if we delete first element, we need to move left pointer.
+        if (k == 0) {
+            left = tmp.next;
+            tmp.next = null;
+            return tmp.item;
+        }
+
+        // if we delete last element, we need to move right pointer.
+        if (k == n) {
+            tmp = right;
+            right = tmp.prev;
+            tmp.prev = null;
+            return tmp.item;
+        }
+
         while (k != 0) {
             tmp = tmp.next;
             k--;
@@ -66,32 +84,47 @@ public class GeneralizedQueueLinked<T> {
 
         @Override
         public String toString() {
-            return "Node{" +
-                    "item=" + item +
-                    ", next=" + next +
-                    '}';
+            return "Node{item=" + item + ", next=" + next + '}';
         }
     }
 
     @Override
     public String toString() {
-        return "GeneralizedQueueLinked{" +
-                "left=" + left +
-                '}';
+        return "GQL{left=" + left + '}';
     }
 
     public static void main(String[] args) {
-        GeneralizedQueueLinked<Integer> queue = new GeneralizedQueueLinked<>();
-        queue.insert(1);
-        queue.insert(2);
-        queue.insert(3);
-        queue.insert(4);
-        System.out.println(queue);
+        System.out.println("Running test for GeneralizedQueueLinked.");
+        GeneralizedQueueLinked<Integer> queue = generateQueue(15);
+        if (queue.isEmpty())
+            throw new RuntimeException("isEmpty");
 
-        System.out.println(queue.delete(1));
-        System.out.println(queue.delete(1));
+        for (int i = 0; i < 15; i++)
+            if (queue.delete(0) != i)
+                throw new RuntimeException(String.valueOf(i));
 
-        System.out.println(queue);
+        if (!queue.isEmpty())
+            throw new RuntimeException("!isEmpty");
+
+        queue = generateQueue(15);
+        if (queue.delete(5) != 5)
+            throw new RuntimeException("delete != 6");
+        if (queue.delete(11) != 12)
+            throw new RuntimeException("delete != 12");
+        if (queue.delete(12) != 14)
+            throw new RuntimeException("delete != 14");
+        queue.insert(33);
+        if (queue.delete(12) != 33)
+            throw new RuntimeException("delete != 33");
+        if (queue.delete(100) != null)
+            throw new RuntimeException("delete != null");
     }
 
+    @SuppressWarnings("SameParameterValue")
+    private static GeneralizedQueueLinked<Integer> generateQueue(int n) {
+        GeneralizedQueueLinked<Integer> queue = new GeneralizedQueueLinked<>();
+        for (int i = 0; i < n; i++)
+            queue.insert(i);
+        return queue;
+    }
 }
